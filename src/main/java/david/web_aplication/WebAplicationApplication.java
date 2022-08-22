296282e841cd0f9959a36d11698ae747d7a9eed6
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2022-08-22 20:00:04 +0200

    Initial commit

---
+++ src/main/java/david/web_aplication/WebAplicationApplication.java
@@ -0,0 +1,13 @@
+package david.web_aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAplicationApplication.class, args);
+	}
+
+}
